b59c6f4658070566b05dd6c8e9e4cb8086a50dcf
sc090410 <courage090410@gmail.com>
2017-05-05 00:04:28 +0800

    Update functionC.cpp

--- functionC.cpp
+++ functionC.cpp
@@ -2,51 +2,40 @@
 using namespace std;
 
 int **transInfo = new int*[3];
-for(int i = 0; i < 2; i++)
-{
+for(int i = 0; i < 2; i++){
 	transInfo[i] = new int[storeNum];
-	for(int j = 0; j < 2; j++)
-	{
+	for(int j = 0; j < 2; j++){
 		if(i == 0 && j == 0)
 		transInfo[i][j] = -1;
 		transInfo[i][j] = 0;
-	}
+	}//record the number of store & center
 }
-int deliver(int storeNum, int centerNum, bool *builtSto, bool *builtCen, int **profitTable, int **storeInfo, int **centerInfo, int **transInfo, int time);
-int transAm(int storeNum, int centerNum, bool *builtSto, bool *builtCen, int **profitTable, int **storeInfo, int **centerInfo, int **transInfo)
-{
+int deliver(int storeNum, int centerNum, bool *storeSet, bool *centerSet, int **profitTable, int **storeInfo, int **centerInfo, int **transInfo, int time);
+int transAm(int storeNum, int centerNum, bool *storeSet, bool *centerSet, int **profitTable, int **storeInfo, int **centerInfo, int **transInfo){
 	int transProfit = 0, time = 0;
-	while(deliver(storeNum, centerNum, builtSto, builtCen, profitTable, storeInfo, centerInfo, transInfo, time) == true)
-	{
-		transProfit += deliver(storeNum, centerNum, builtSto, builtCen, profitTable, storeInfo, centerInfo, transInfo, time);
+	while(deliver(storeNum, centerNum, storeSet, centerSet, profitTable, storeInfo, centerInfo, transInfo, time) == true){
+		transProfit += deliver(storeNum, centerNum, storeSet, centerSet, profitTable, storeInfo, centerInfo, transInfo, time);
 		time++;
-	}
-	transInfo[time][0] = -1;
+	}//if profit != 0, keep adding gross profit
+	transInfo[time][0] = -1;//to know that before[i][0] == -1, there are the record of transmit
 	return transProfit;
 }
 
-int deliver(int storeNum, int centerNum, bool *builtSto, bool *builtCen, int **profitTable, int **storeInfo, int **centerInfo, int transInfo[2])
-{
+int deliver(int storeNum, int centerNum, bool *storeSet, bool *centerSet, int **profitTable, int **storeInfo, int **centerInfo, int transInfo[2]){
 	int transStore = -1, transCenter = -1, profit = 0, category = 0;
-	for(int i = 0; i < storeNum; i++)
-	{
-		for(int j = 0; j < centerNum; j++)
-		{
+	for(int i = 0; i < storeNum; i++){
+		for(int j = 0; j < centerNum; j++){
 			int temPro = 0, type = 0;
-			if(builtSto[i] && builtCen[j] && profitTable[i][j] && storeInfo[i][2] && centerInfo[j][2])
-			{
-				if(storeInfo[i][2] >= centerInfo[j][2])
-				{
+			if(storeSet[i] && centerSet[j] && profitTable[i][j] && storeInfo[i][2] && centerInfo[j][2]){ //if the store & center are built and still have demand & capacity
+				if(storeInfo[i][2] >= centerInfo[j][2]){
 					temPro = profitTable[i][j] * centerInfo[j][2];
-					type = 0;
+					type = 0;//if demand >= capacity, to know which amount is used
 				}
-				else
-				{
+				else{
 					temPro = profitTable[i][j] * storeInfo[i][2];
 					type = 1;
 				}
-				if(temPro > profit)
-				{
+				if(temPro > profit){
 					profit = temPro;
 					transStore = i;
 					transCenter = j;
@@ -56,8 +45,7 @@ int deliver(int storeNum, int centerNum, bool *builtSto, bool *builtCen, int **p
 		}
 	}
 	
-	if(profit > 0)
-	{
+	if(profit > 0){
 		transInfo[time][0] = category;
 		transInfo[time][1] = transStore;
 		transInfo[time][2] = transCenter;
