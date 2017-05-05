6b7d0141a5c4fe247dbbc8299f9964eeb517c588
scarlettchia <scarlettchen1027@gmail.com>
2017-05-06 00:21:29 +0800

    Add files via upload

---
+++ function B outside.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void newStore(int storeNum, int cost, bool* storeSet, bool* centerSet, int** profitTable, int** storeInfo, int** centerInfo){
+
+	bool* tempStoreSet= new bool[storeNum];
+	for(int i=0; i<storeNum; i++){
+		tempStoreSet[i]=storeSet[i];
+	}
+
+	int netProfitB=0;
+	int maxProfitB=-1;
+	int storeOfMaxB=-1;
+	for(int i=0; i<storeNum; i++){
+		if(tempStoreSet[i]==false){
+			if(profitTable[i][j]!=0 && centerSet[j]==true){
+				if(storeInfo[i][2]<=centerInfo[i][2]){
+					netProfitB+=(profitTable[i][j]*storeInfo[i][2]);
+					netProfitB-=storeInfo[i][3];
+					if(netProfitB>maxProfitB){
+						maxProfitB=netProfitB;
+						storeOfMaxB=i;
+					}		
+				}
+			}
+		}
+	}
+}
+
+int netProfitOfNewStore(int storeNum, int cost, bool* storeSet, bool* centerSet, int** profitTable, int** storeInfo, int** centerInfo){
+	newStore(storeNum, cost, *storeSet, *centerSet, **profitTable, **storeInfo, **centerInfo);
+	return maxProfitB;
+}
+
+int numOfNewStore(int storeNum, int cost, bool* storeSet, bool* centerSet, int** profitTable, int** storeInfo, int** centerInfo){
+	newStore(storeNum, cost, *storeSet, *centerSet, **profitTable, **storeInfo, **centerInfo);
+	return storeOfMaxB;
+}
+
+int transOfNewStore(int storeNum, int cost, bool* storeSet, bool* centerSet, int** profitTable, int** storeInfo, int** centerInfo){
+	newStore(storeNum, cost, *storeSet, *centerSet, **profitTable, **storeInfo, **centerInfo);
+	return storeInfo[storeOfMaxB][2];
+}
