13dfb06c2585900f11d223a8c5875601efd8f703
scarlettchia <scarlettchen1027@gmail.com>
2017-05-05 23:47:33 +0800

    Update function B.cpp

--- function B.cpp
+++ function B.cpp
@@ -1,14 +1,41 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
-for(int i=0; i<storeNum; i++){
-	if(storeSet[i]==false){
-		if(profitTable[i][j]!=0 && centerSet[j]==true){
-			if(storeInfo[i][2]<=centerInfo[i][2]){
-					netProfit+=(storeInfo[i][4]-(cost*abs(storeInfo[i][0]-centerInfo[j][0])+abs(storeInfo[i][1]-centerInfo[j][1])));
-					netProfit-=storeInfo[i][3];
-					numOfStores++;
-					storeSet[i]=true;		
+void newStore(int storeNum, int cost, bool* storeSet, bool* centerSet, int** profitTable, int** storeInfo, int** centerInfo){
+
+	int* tempStoreSet= new int[storeNum];
+	for(int i=0; i<storeNum; i++){
+		for(int j=0; j<storeNum; j++){
+			tempStoreSet[j]=storeSet[i];
+		}
+	}
+
+	int netProfitB=0;
+	int maxProfitB=-1;
+	int storeOfMaxB=-1;
+	for(int i=0; i<storeNum; i++){
+		if(tempStoreSet[i]==false){
+			if(profitTable[i][j]!=0 && centerSet[j]==true){
+				if(storeInfo[i][2]<=centerInfo[i][2]){
+					netProfitB+=(storeInfo[i][4]-(cost*abs(storeInfo[i][0]-centerInfo[j][0])+abs(storeInfo[i][1]-centerInfo[j][1])));
+					netProfitB-=storeInfo[i][3];
+					if(netProfitB>maxProfitB){
+						maxProfitB=netProfitB;
+						storeOfMaxB=i;
+					}		
+				}
 			}
 		}
 	}
 }
 
+int netProfitOfNewStore(int storeNum, int cost, bool* storeSet, bool* centerSet, int** profitTable, int** storeInfo, int** centerInfo){
+	newStore(storeNum, cost, *storeSet, *centerSet, **profitTable, **storeInfo, **centerInfo);
+	return maxProfitB;
+}
+
+int numOfNewStore(int storeNum, int cost, bool* storeSet, bool* centerSet, int** profitTable, int** storeInfo, int** centerInfo){
+	newStore(storeNum, cost, *storeSet, *centerSet, **profitTable, **storeInfo, **centerInfo);
+	return storeOfMaxB;
+}
