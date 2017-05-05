985b5f2a9ed7c4c0f1810644c311818d5b81c65c
payko <flower.payko00@gmail.com>
2017-05-06 01:01:52 +0800

    Add files via upload

--- functionA.cpp
+++ functionA.cpp
@@ -1,111 +1,46 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int setTransAm(int cap, int dem);
-void setTable(int **profitTable, int storeNum, int residualCap, int **storeInfo, bool *storeSet);
-int findBestI( int **profit, int center, int storeNum );
-int *findRelatStore(int *bestJ, int **profitTable, int storeNum);
-int* findBestJ( int  **centerInfo, int **storeInfo, int **profitTable, int centerNum, int storeNum, bool *storeSet, bool *centerSet);
-
-
-int* findBestJ( int  **centerInfo, int **storeInfo, int **profitTable, int centerNum, int storeNum, bool *storeSet, bool *centerSet)
-{
-	int relatStoreNum = 0;
-	int maxProfit = 0;
-	int bestCen = 0;
+functionA(int storeNum, int centerNum, int cost, bool *storeSet, bool *centerSet, int **profitTable, int **storeInfo, int **centerInfo){
+	int *decide = new int[5]; //maxNetProfit , center , store , build(1)/trans(0) , transAm
+	//initialize
+	for( int i = 0; i < 5; i++ ){
+		decide[i] = 0;
+	}
 	
-	for( int j = 0; j < centerNum; j++ )
-	{
-		if(centerSet[j] == false)
-		{
-			int profit = 0;
-			int residualCap = centerInfo[j][2];
+	for( int j = 0; j < centerNum; j++ ){
+		if( centerSet[j] == false ){
+			centerSet[j] = true;  //assume it is set
 			
-			
-			setTable(profitTable, storeNum, centerNum, residualCap, storeInfo, storeSet);
-			int bestI = findBestI( profitTable, j, storeNum );
-			int a = 0;
-			while( residualCap && bestI != -1) //center still have capacity
-			{
-				int transAm = setTransAm(residualCap, storeInfo[bestI][2]);
-				residualCap -= transAm;
-				profit += profitTable[bestI][j] * transAm;
-				profit -= storeInfo[bestI][3];
-			
-				setTable(profitTable, storeNum, centerNum, residualCap, storeInfo, storeSet);
-				bestI = findBestI( profitTable, j, storeNum );
-				a++;	
+			newStore( j, storeNum, storeSet, centerSet, profitTable, storeInfo, centerInfo);  //call functionB
+			int profitB = netProfitOfNewStore(j, storeNum, centerSet, profitTable, storeInfo, centerInfo );
+			int *transInfo = new int[2];  //prepare for functionC
+			for( int i = 0; i < 2; i++ ){
+				transInfo[i] = 0;
 			}
-			profit -= centerInfo[j][3];
-			if( profit > maxProfit )
-			{
-				maxProfit = profit;
-				bestCen = j;
-				relatStoreNum = a;
+			int profitC = functionC();  //call functionC
+			int profit = max( profitB, profitC );
+			
+			if( profit > decide[0] ){
+				decide[0] = max(profitB,profitC) - centerInfo[j][3];  //maxNetProfit
+				decide[1] = j;  //center
+				if( profit == profitB ){
+					//decided by functionB
+					decide[2] = numOfNewStore(j, storeNum, centerSet, profitTable, storeInfo, centerInfo );  //store
+					decide[3] = 1;  //build
+					decide[4] = transOfNewStore;  //transAm
+				}
+				else{
+					//decided by functionC
+					decide[2] = transInfo[1];  //store
+					decide[3] = 0;  //trans
+					decide[4] = transInfo[0];  //transAm
+				}
 			}
-		}	
-	}
-	int *bestJ = new int[relatStoreNum +2];
-	//first for netProfit,second for bestJ's index,third for relatStoreNum,others for relative I's index
-	bestJ[0] = maxProfit;
-	bestJ[1] = bestCen;
-	bestJ[2] = relatStoreNum;
-	for( int a = 0; a < relatStoreNum; a++)
-		bestJ[a+3] = 0;
-	return bestJ;	
-}
-
-int *findRelatStore(int *bestJ, int **profitTable, int storeNum)
-{
-	for( int a = 0; a < bestJ[2]; a++ )
-	{
-		bestJ[a + 3] = findBestI( profitTable, bestJ[1], storeNum );
-	}
-	
-	return bestJ;
-}
-	
-
-
-int setTransAm(int cap, int dem)
-{
-	int transAm = (cap > dem? dem:cap);
-	
-	return transAm;
-}
-
-void setTable(int **profitTable, int storeNum, int centerNum, int residualCap, int **storeInfo, bool *storeSet)
-{
-	//consider transAm & fixed cost of store
-	int *transAm = new int[storeNum];
-	for( int i = 0; i < storeNum; i++ )
-	{
-		transAm[i] = setTransAm(residualCap, storeInfo[i][2]);
-	}
-	for( int j = 0; j < centerNum; j++)
-		for( int i = 0; i < storeNum; i++)
-		{
-			profitTable[i][j] = profitTable[i][j] * transAm[i];
-			if( storeSet[i] == false )
-			{
-				profitTable[i][j] -= storeInfo[i][3];
-			}		
-		}
-}
-
-int findBestI( int **profit, int center, int storeNum )
-{
-	int max = 0;
-	for( int i = 0; i < storeNum; i++)
-	{
-		if( profit[i][center] > profit[max][center] )
-		{
-			max = i;
+			
+			centerSet[j] = false;  //delete assumption
 		}
 	}
-	if(profit[max][center] == 0)
-		return -1;
-		
-	profit[max][center] = 0;
-		return max;
-}
+	return decide;
+} 
