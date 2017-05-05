3e8ebe026288c89ebfa3133d2319c725aaa9a124
sc090410 <courage090410@gmail.com>
2017-05-06 01:06:32 +0800

    Add files via upload

---
+++ functionC-A.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int transInfo[2] = {0};
+//bestJ = bestJ[1] = bestCen,centerCapa = centerInfo[bestCen][2];
+int nowYouSeeMe(int storeNum, int bestJ, bool *storeSet, int **profitTable, int **storeInfo, int centerCapa, int transInfo[2]){
+	int *storeDem = new int[storeNum];
+	for(int i = 0; i < storeNum; i++){
+		storeDem[i] = storeInfo[i][2];
+	}
+
+	int transStore = -1, transAm = 0, profit = 0, category = 0;
+	for(int i = 0; i < storeNum; i++){
+		int temPro = 0, type = 0;
+		if(storeSet[i] && profitTable[i][bestJ] && storeDem[i] && centerCapa){ //if the store & center are built and still have demand & capacity
+			if(storeDem[i] >= centerCapa){
+				temPro = profitTable[i][bestJ] * centerCapa;
+				type = 0;//if demand >= capacity, to know which amount is used
+			}
+			else{
+				temPro = profitTable[i][bestJ] * storeDem[i];
+				type = 1;
+			}
+			if(temPro > profit){
+				profit = temPro;
+				transStore = i;
+				category = type;
+				if(category){
+					transAm = storeDem[transStore];
+				}
+				else{
+					transAm = centerCapa;
+				}
+			}
+		}
+	}
+	
+	if(profit > 0){
+		transInfo[0] = transAm;
+		transInfo[1] = transStore;
+	}
+	
+	delete[]storeDem;
+	return profit;
+}
