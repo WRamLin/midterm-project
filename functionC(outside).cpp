397a7789bd27bc9f3af8060ed25b0c9a991f5bbb
sc090410 <courage090410@gmail.com>
2017-05-06 18:45:58 +0800

    Update functionC(outside).cpp

--- functionC(outside).cpp
+++ functionC(outside).cpp
@@ -15,31 +15,34 @@ int catchMeIfYouCan(int storeNum, int centerNum, bool *storeSet, bool *centerSet
 	}
 	int transStore = -1, transCenter = -1, transAm = 0, profit = 0, category = 0;
 	for(int i = 0; i < storeNum; i++){
-		for(int j = 0; j < centerNum; j++){
-			int temPro = 0, type = 0;
-			if(storeSet[i] && centerSet[j] && profitTable[i][j] && storeDem[i] && centerCapa[j]){ //if the store & center are built and still have demand & capacity
-				if(storeDem[i] >= centerCapa[j]){
-					temPro = profitTable[i][j] * centerCapa[j];
-					type = 0;//if demand >= capacity, to know which amount is used
-				}
-				else{
-					temPro = profitTable[i][j] * storeDem[i];
-					type = 1;
-				}
-				if(temPro > profit){
-					profit = temPro;
-					transStore = i;
-					transCenter = j;
-					category = type;
-					if(category){
-						transAm = storeDem[transStore];
+		if(storeSet[i]){
+			for(int j = 0; j < centerNum; j++){
+				int temPro = 0, type = 0;
+				if(centerSet[j] && profitTable[i][j] && storeDem[i] && centerCapa[j]){ //if the store & center are built and still have demand & capacity
+					if(storeDem[i] >= centerCapa[j]){
+						temPro = profitTable[i][j] * centerCapa[j];
+						type = 0;//if demand >= capacity, to know which amount is used
 					}
 					else{
-						transAm = centerCapa[transCenter];
+						temPro = profitTable[i][j] * storeDem[i];
+						type = 1;
+					}
+					if(temPro > profit){
+						profit = temPro;
+						transStore = i;
+						transCenter = j;
+						category = type;
+						if(category){
+							transAm = storeDem[transStore];
+						}
+						else{
+							transAm = centerCapa[transCenter];
+						}
 					}
 				}
 			}
 		}
+		
 	}
 	
 	if(profit > 0){
