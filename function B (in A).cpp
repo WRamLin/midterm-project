e8f4e8126594c9da4e523c45aba95eb6419cbbfb
sc090410 <courage090410@gmail.com>
2017-05-06 19:11:18 +0800

    Update function B (in A).cpp

--- function B (in A).cpp
+++ function B (in A).cpp
@@ -11,23 +11,34 @@ int* newStore(int j, int storeNum, int cost, bool* storeSet, bool* centerSet, in
 	int netProfitB=0;
 	int maxProfitB=-1;
 	int storeOfMaxB=-1;
+	int category = -1;
 	for(int i=0; i<storeNum; i++){
 		if(storeSet[i]==false){
+			int tempCate = 0;
 			if(profitTable[i][j]!=0 && centerSet[j]==true){
 				if(storeInfo[i][2]<=centerInfo[i][2]){
 					netProfitB+=(profitTable[i][j]*storeInfo[i][2]);
+					netProfitB-=storeInfo[i][3];	
+				}
+				else{
+					netProfitB+=(profitTable[i][j]*centerInfo[j][2]);
 					netProfitB-=storeInfo[i][3];
-					if(netProfitB>maxProfitB){
-						maxProfitB=netProfitB;
-						storeOfMaxB=i;
-					}		
+					tempCate = 1;
+				}
+				if(netProfitB>maxProfitB){
+					maxProfitB=netProfitB;
+					storeOfMaxB=i;
+					category = tempCate;
 				}
 			}
 		}
 	}
 	ansB[0]=maxProfitB;
 	ansB[1]=storeOfMaxB;
-	ansB[2]=storeInfo[storeOfMaxB][2];
+	if(category)
+		ansB[2] = centerInfo[j][2];
+	else
+		ansB[2]=storeInfo[storeOfMaxB][2];
 	
 	return *ansB;
 }
