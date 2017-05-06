b7f4fc1f6a62968e5c63fe75092c3743f6bd5d31
scarlettchia <scarlettchen1027@gmail.com>
2017-05-06 18:51:01 +0800

    Update function B outside.cpp

--- function B outside.cpp
+++ function B outside.cpp
@@ -3,24 +3,21 @@ using namespace std;
 
 void newStore(int storeNum, int cost, bool* storeSet, bool* centerSet, int** profitTable, int** storeInfo, int** centerInfo){
 
-	bool* tempStoreSet= new bool[storeNum];
-	for(int i=0; i<storeNum; i++){
-		tempStoreSet[i]=storeSet[i];
-	}
-
 	int netProfitB=0;
 	int maxProfitB=-1;
 	int storeOfMaxB=-1;
 	for(int i=0; i<storeNum; i++){
-		if(tempStoreSet[i]==false){
-			if(profitTable[i][j]!=0 && centerSet[j]==true){
-				if(storeInfo[i][2]<=centerInfo[i][2]){
-					netProfitB+=(profitTable[i][j]*storeInfo[i][2]);
-					netProfitB-=storeInfo[i][3];
-					if(netProfitB>maxProfitB){
-						maxProfitB=netProfitB;
-						storeOfMaxB=i;
-					}		
+		for(int j=0; j<storeNum; j++){
+			if(storeSet[i]==false){
+				if(profitTable[i][j]!=0 && centerSet[j]==true){
+					if(storeInfo[i][2]<=centerInfo[i][2]){
+						netProfitB+=(profitTable[i][j]*storeInfo[i][2]);
+						netProfitB-=storeInfo[i][3];
+						if(netProfitB>maxProfitB){
+							maxProfitB=netProfitB;
+							storeOfMaxB=i;
+						}		
+					}
 				}
 			}
 		}
