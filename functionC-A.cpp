6cec7052d3ffe47bdac08bf08d774cf00ed28645
sc090410 <courage090410@gmail.com>
2017-05-07 18:34:21 +0800

    Update functionC-A.cpp

--- functionC-A.cpp
+++ functionC-A.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std;
 
-int transInfo[2] = {0};
+int transInfoC[2] = {0};
 //bestJ = bestJ[1] = bestCen,centerCapa = centerInfo[bestCen][2];
-int nowYouSeeMe(int storeNum, int bestJ, bool *storeSet, int **profitTable, int **storeInfo, int centerCapa, int transInfo[2]){
+int nowYouSeeMe(int storeNum, int bestJ, bool *storeSet, int **profitTable, int **storeInfo, int centerCapa, int transInfoC[2]){
 	int *storeDem = new int[storeNum];
 	for(int i = 0; i < storeNum; i++){
 		storeDem[i] = storeInfo[i][2];
@@ -36,8 +36,8 @@ int nowYouSeeMe(int storeNum, int bestJ, bool *storeSet, int **profitTable, int 
 	}
 	
 	if(profit > 0){
-		transInfo[0] = transAm;
-		transInfo[1] = transStore;
+		transInfoC[0] = transAm;
+		transInfoC[1] = transStore;
 	}
 	
 	delete[]storeDem;
